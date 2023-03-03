8aeea78e655a5b27541b8285f1f02a5b7945e292
DylMcn <117716229+DylMcn@users.noreply.github.com>
2023-03-03 11:32:32 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class App extends React.Component{
-  render(){
+export class App extends Component {
+  render() {
     return (
       <div>
+        <h1>Hello, world!</h1>
+        <p>This is a React class component with only a render method.</p>
       </div>
-    )
-
+    );
   }
 }
+
 ReactDOM.render(<App/>, document.getElementById('root'))
 
 
